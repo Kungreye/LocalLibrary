6e96204b19f861faeca12070832152b0dbe14206
Kungreye <kungreye@sina.com>
2019-10-25 01:04:00 +0800

    generic list and detail views

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from django.shortcuts import render
+from django.views import generic
 
 from catalog.models import Book, Author, BookInstance, Genre
 
@@ -17,3 +18,21 @@ def index(request):
         "num_authors": num_authors,
     }
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
